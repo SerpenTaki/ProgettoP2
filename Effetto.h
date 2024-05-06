c6f20513bef0e718547875d14618301da2319023
SerpenTaki <gabrielecorubolo@gmail.com>
2024-05-06 15:14:05 +0200

    update

--- Effetto.h
+++ Effetto.h
@@ -11,7 +11,6 @@ private:
 public:
     Effetto();
     ~Effetto();
-
     double calcolaDanno();
 };
 
--- Sensore.h
+++ Sensore.h
@@ -3,16 +3,30 @@
 
 #include <string>
 
+using std::string;
 
 class Sensore {
 private:
+  //parametri che descrivono il sensore generico
   double danno;
+  bool status; //indica se il sensore è attivo
+  string descrizione;
+  unsigned int percorso_immagine;
+  int probHit;
+protected:
+  Sensore(double danno, bool status, string descrizione, unsigned int percorso_immagine, int probHit); //costruttore di default
 public:
-  Sensore();
-  ~Sensore();
-  virtual bool isDead() const;
-  virtual double getDanno() const;
-  virtual double calcolaDanno();
+  //distruttore virtuale
+  virtual ~Sensore();
+  //Metodi di Set
+  void setStatus(bool status);
+  void modStatus(bool status);
+  //Metodi di Get
+  double getDanno() const;
+  double calcolaDanno() const;
+  string getDescrizione() const;
+  //Altri metodi
+  bool checkStatus() const;
 };
 
 #endif
