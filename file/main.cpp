d46a19eac1e637c48bae891d2a530141741f9df8
EjoGrejo <elio.greggio@gmail.com>
2024-05-11 18:59:00 +0200

    cristo

--- file/main.cpp
+++ file/main.cpp
@@ -1,4 +1,23 @@
+#include "qboxlayout.h"
+#include "qwidget.h"
 #include <QApplication>
 #include <QLabel>
+#include <QBoxLayout>
 
-int main(int argc, char *argv[]) {}
+class megamerda:public QWidget{
+public: 
+    megamerda(QWidget*  parent=nullptr): QWidget(parent){
+        QVBoxLayout* main = new QVBoxLayout(this);
+        main->addWidget(new QLabel("Caccamerda"));
+        main->addWidget(new QLabel("Merdaculo"));
+        
+        
+    }
+};
+    
+int main(int argc, char *argv[]) {
+    QApplication App(argc, argv);
+    megamerda m;
+    m.show();
+    return App.exec();
+}
