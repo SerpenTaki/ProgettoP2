8950800fba4c786b39c792ca69690cd3b733805b
EjoGrejo <elio.greggio@gmail.com>
2024-05-12 11:05:20 +0200

    esempi fatti com michele

--- file/main.cpp
+++ file/main.cpp
@@ -1,23 +1,58 @@
-#include "qboxlayout.h"
-#include "qwidget.h"
+// #include "QtCore/qtmetamacros.h"
+// #include "qboxlayout.h"
+// #include "qgroupbox.h"
+// #include "qlabel.h"
+// #include "qpushbutton.h"
+// #include "qwidget.h"
 #include <QApplication>
-#include <QLabel>
 #include <QBoxLayout>
+#include <QGroupBox>
+#include <QLabel>
+#include <QPushButton>
 
+/*
 class megamerda:public QWidget{
-public: 
+private:
+    QLabel* magia;
+    QLabel* fisico;
+
+public:
     megamerda(QWidget*  parent=nullptr): QWidget(parent){
         QVBoxLayout* main = new QVBoxLayout(this);
-        main->addWidget(new QLabel("Caccamerda"));
-        main->addWidget(new QLabel("Merdaculo"));
-        
-        
+        QVBoxLayout* l_sensore = new QVBoxLayout;
+        QGroupBox* sensore = new QGroupBox;
+        QVBoxLayout* l_sensore2 = new QVBoxLayout;
+        QGroupBox* sensore2 = new QGroupBox;
+        magia = new QLabel("Magia");
+        fisico = new QLabel("Fisico");
+        QPushButton* edit = new QPushButton("Edit");
+        QPushButton* edit2 = new QPushButton("Edit");
+        l_sensore->addWidget(magia);
+        l_sensore->addWidget(edit);
+        l_sensore2->addWidget(fisico);
+        l_sensore2->addWidget(edit2);
+        sensore->setLayout(l_sensore);
+        sensore2->setLayout(l_sensore2);
+        main->addWidget(sensore);
+        main->addWidget(sensore2);
+        connect(edit, &QPushButton::clicked, this, &megamerda::edit_magia);
+        connect(edit2, &QPushButton::clicked, this, &megamerda::edit_fisico);
     }
+    public slots:
+        void edit_magia(){
+            magia->setText("Orcamadonna");
+        }
+        void edit_fisico(){
+            fisico->setText("Stronziddio");
+        }
+
 };
-    
+
+*/
+
 int main(int argc, char *argv[]) {
-    QApplication App(argc, argv);
-    megamerda m;
-    m.show();
-    return App.exec();
+  QApplication App(argc, argv);
+  megamerda m;
+  m.show();
+  return App.exec();
 }
