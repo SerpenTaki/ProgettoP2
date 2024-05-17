9d3e09e3e1744b1d3dc3e8f8958ebaaff0a3facf
EjoGrejo <elio.greggio@gmail.com>
2024-05-17 18:13:07 +0200

    ancora incompleto

--- qt_files/visitor_new_sensor.cpp
+++ qt_files/visitor_new_sensor.cpp
@@ -1,8 +1,26 @@
 
 #include "headers/visitor_new_sensor.h"
+#include <QLabel>
+#include <QWidget>
 
-void visitor_new_sensor::visit(erba *er) {}
-void visit(acqua *aq) {}
-void visit(fisico *fi) {}
-void visit(fuoco *fu) {}
-void visit(sacro *sa) {}
+void visitor_new_sensor::visit(erba *er) {
+  QWidget *sensor = new QWidget;
+  QLabel *tipo = new QLabel(QString::fromStdString(erba::tipo));
+}
+
+void visitor_new_sensor::visit(acqua *aq) {
+  QWidget *sensor = new QWidget;
+  QLabel *tipo = new QLabel(QString::fromStdString(acqua::tipo));
+}
+void visitor_new_sensor::visit(fisico *fi) {
+  QWidget *sensor = new QWidget;
+  QLabel *tipo = new QLabel(QString::fromStdString(fisico::tipo));
+}
+void visitor_new_sensor::visit(fuoco *fu) {
+  QWidget *sensor = new QWidget;
+  QLabel *tipo = new QLabel(QString::fromStdString(fuoco::tipo));
+}
+void visitor_new_sensor::visit(sacro *sa) {
+  QWidget *sensor = new QWidget;
+  QLabel *tipo = new QLabel(QString::fromStdString(sacro::tipo));
+}
